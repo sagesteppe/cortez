5689ab4686490c47e636850dda69434aeaa7ab50
sagesteppe <reedbenkendorf27@gmail.com>
2025-05-11 20:43:22 -0500

    date format

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,7 +7,9 @@ library(usethis)
 library(data.table)
 
 places <- read.csv('Places.csv') |>
-  st_as_sf(coords = c('longitude', 'latitude'), crs = 4326)
+  st_as_sf(coords = c('longitude', 'latitude'), crs = 4326) |>
+  mutate(across(starts_with('date'), ~ as.Date(.x, format = "%m-%d-%Y"))) |>
+  arrange(date_arrive)
 
 ggplot() +
   geom_sf(data = places)
@@ -19,20 +21,20 @@ usethis::use_data(places, overwrite = TRUE)
 ## Route of the Western Flyer!!!
 
 # essentially I digitized the map route in the book, and freehand draw it out
-# in qgis using points. The points then get attached here. 
+# in qgis using points. The points then get attached here.
 
 # these are entered in an odd fashion, I made two geopackages rather than two layers in one gpkg
 
 route <- bind_rows(
   st_read('Trip_part-1.gpkg', quiet = T),
   st_read('Trip_part-2.gpkg', quiet = T) |>
-    rename(geom = geometry) 
-) %>% 
+    rename(geom = geometry)
+) %>%
   filter(!st_is_empty(.)) |>
   summarise(do_union = FALSE) |>
   st_cast("LINESTRING")
 
-# now we will write out the combined parts, and make sure they look ok in qgis. 
+# now we will write out the combined parts, and make sure they look ok in qgis.
 
 st_write(route, 'route-ls-1.gpkg', append = F)
 # I re ran this a handful of times (intermediates not saved) and modified the original
@@ -43,12 +45,12 @@ st_write(chai_route, 'route-ls-chai.gpkg', append = F)
 unlink('route-ls-1.gpkg')
 
 rm(route)
-# create segments to assign to locations later. 
+# create segments to assign to locations later.
 
-chai_route <- st_transform(chai_route, 32612) 
+chai_route <- st_transform(chai_route, 32612)
 segs <- (as.numeric(st_length(chai_route)) / 6436)
-sample_points <- st_line_sample(chai_route, n = segs, type = "regular") 
-split_lines <- lwgeom::st_split(chai_route, sample_points) %>% 
+sample_points <- st_line_sample(chai_route, n = segs, type = "regular")
+split_lines <- lwgeom::st_split(chai_route, sample_points) %>%
   st_collection_extract('LINESTRING')
 
 split_lines <- nngeo::st_segments(split_lines, progress = TRUE)
@@ -59,19 +61,19 @@ st_write(split_lines, 'route-split.gpkg', append = F)
 unlink('route-ls-chai.gpkg')
 
 rm(segs, sample_points, chai_route)
-# and more iterations on the above to get a product I liked. 
+# and more iterations on the above to get a product I liked.
 
-## now we need to append the 'days' to the sections of the route. 
-# part of this data set is about having times for the line segments. 
+## now we need to append the 'days' to the sections of the route.
+# part of this data set is about having times for the line segments.
 
 
 # this will be slighty messy code, but the general idea is we identify the route
-# line segment which is closest to a port and then snap dates between the ports. 
+# line segment which is closest to a port and then snap dates between the ports.
 
 # identify route directions from the route-ls-chai data set. we do this by
-# identifying the nearest 'trip-part' points 
+# identifying the nearest 'trip-part' points
 
-route_dates <- places[4:25,'location.english'] |> 
+route_dates <- places[4:25,'location.english'] |>
   st_drop_geometry() |>
   rename(Destination = location.english) |>
   mutate(
@@ -80,7 +82,7 @@ route_dates <- places[4:25,'location.english'] |>
   mutate(
     Start = lag(End, n = 1L)+1, .before = End,
     Start = case_when(
-      Destination == 'Magdalena Bay' ~ 1, 
+      Destination == 'Magdalena Bay' ~ 1,
       .default = as.numeric(Start)
       )
   )
@@ -90,6 +92,7 @@ route_dates <- data.table(route_dates)
 
 setkey(split_lines, ID)
 setkey(route_dates, Start)
+
 
 route <- route_dates[split_lines, roll = T] |>
   mutate(Destination = if_else(Start > 2814, 'San Diego', Destination)) |>
@@ -106,8 +109,6 @@ usethis::use_data(route, overwrite = TRUE)
 
 ##### Markers
 
-# these are points discussed in the book, generally in passing. 
-
+# these are points discussed in the book, generally in passing.
 
 dat <- read.csv('./species/Site4.csv')
-s
